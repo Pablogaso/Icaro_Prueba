e835f010264ec224aa836735bfb7b7f8d279308c
Pablogaso <pablo.gaso@gmail.com>
2022-11-16 19:21:22 -0300

    gi

---
+++ clase_cuatro.js
@@ -0,0 +1,5 @@
+const restar = (numero1, numero1) => numero1-numero2;
+console.log=restar
+
+
+
---
+++ funciones.js
@@ -0,0 +1,8 @@
+const valor1 = 30;
+const valor2 = 20;
+
+if (valor1 > valor2 ){
+    console.log ("El valor 1 es mayor que el valor2");
+}else{
+    console.log ("El valor 2 es mayor que el valor1")
+}         
